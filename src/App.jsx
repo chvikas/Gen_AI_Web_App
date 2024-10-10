c5f306232d7adca0d48600b356970f62bd6413cd
Vikas Chaudhary <chvikas.ai@gmail.com>
2024-10-10 23:40:13 +0530

    Design Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,60 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [query, setQuery] = useState("")
+  const [noOfQuestions, setNoOfQuestions] = useState(4)
+  const [difficulty, setDifficulty] = useState("")
+
+  const handleQueryInputChange = (e) => {
+    setQuery(e.target.value)
+  }
+
+  const handleNumQuestionsInputChange = (e) => {
+    setNoOfQuestions(e.target.value)
+  }
+
+  const handleDifficultyInputChange = (e) => {
+    setDifficulty(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(query)
+    console.log(noOfQuestions)
+    console.log(difficulty)
+  }
 
   return (
-   <div className='main-container'>
-    <h1>Gen AI Web App</h1>
-    <div className='form-container'>
-      <div>
-        <label>Enter Query:</label>
-        <input className="query-input" type="text" placeholder="Ask me something:"/>
+    <div className='main-container'>
+      <h1>Gen AI Web App</h1>
+      <div className='form-container'>
+        <div>
+          <label>Enter Query:</label>
+          <input type="text" className='query-input' placeholder="Enter Query" 
+          onChange={handleQueryInputChange}/>
+        </div>
+
+        <div>
+          <label>No of Questions: {noOfQuestions}</label>
+          <input type='range' min={1} max={10} className="questions-input" 
+          onChange={handleNumQuestionsInputChange}/>
+        </div>
+        
+        <div>
+          <label>Difficulty:</label>
+          <select className='difficulty-input' onChange={handleDifficultyInputChange}>
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+          </select>
+        </div>
+
+        <button className="submit-button" onClick={handleSubmit}>
+          Generate Questions
+        </button>
       </div>
-
     </div>
-
-   </div>
   );
 }
 
